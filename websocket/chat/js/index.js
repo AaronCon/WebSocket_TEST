1521c1f0de191e3c8eee1b4347e37439accb432b
chenjiawen <8928027+chenjiawen666@user.noreply.gitee.com>
2022-10-11 21:36:35 +0800

    完成了前后端最基本的websocket通讯

--- websocket/chat/js/index.js
+++ websocket/chat/js/index.js
@@ -1,33 +1,22 @@
 ((doc, WebSocket) => {
   const oList = doc.querySelector('#list')
-  const omessage = doc.querySelector('#message')
   const oSend = doc.querySelector('#send')
-  const ws = new WebSocket('ws:localhost:8000')
+  const ws = new WebSocket('ws://localhost:8000')
 
   const init = () => {
     bindEvent()
   }
   const bindEvent = () => {
     oSend.addEventListener('click', handerEnterSend)
-    ws.addEventListener('open', handleOpen)
-    ws.addEventListener('close', handleClose)
-    ws.addEventListener('error', handleError)
-    ws.addEventListener('message', handleMessage)
-  }
-  const handerEnterSend = () => {
-    console.log('SendMessage')
-  }
-  const handleOpen = (e) => {
-    console.log('websocket open', e)
-  }
-  const handleClose = (e) => {
-    console.log('websocket close', e)
-  }
-  const handleError = (e) => {
-    console.log('websocket error', e)
-  }
-  const handleMessage = (e) => {
-    console.log('websocket message', e)
+    
+    function handerEnterSend() {
+      console.log("连接开始")
+      ws.onopen = function() {
+        let message = doc.querySelector('#message').value
+        console.log(message)
+        ws.send(message)
+      }()
+    }
   }
 
   init()
--- websocket/server/index.js
+++ websocket/server/index.js
@@ -1,34 +1,37 @@
-const WebSocket = require('ws')
-((WebSocket) => {
+(() => {
+  const WebSocket = require('ws')
   // ws:localhost:8000
-  const WebSocketServer = WebSocket.Server;
-  const server = new WebSocketServer({
-    port: 8000
-  })
+  const ws = new WebSocket.Server({port: 8000})
   const init = () => {
     bindEvent()
   }
   function bindEvent() {
-    server.on('open', handleOpen)
-    server.on('close', handleClose)
-    server.on('error', handleError)
-    server.on('connection', handleConnection)
+    ws.on('connection', function(wss) {
+      wss.on('message', function(message) {
+        console.log('客户端发送的消息为'+ message)
+      })
+    })
+    // server.on('open', handleOpen)
+    // server.on('close', handleClose)
+    // server.on('error', handleError)
+    // ws.on('connection', handleConnection(wss))
   }
-  function handleOpen() {
-    console.log('WebSocket Open')
-  }
-  function handleClose() {
-    console.log('WebSocket Close')
-  }
-  function handleError() {
-    console.log('WebSocket Error')
-  }
-  function handleConnection(ws) {
-    console.log('WebSocket Connetion')
-    ws.on('message', handleMessage)
-  }
-  function handleMessage() {
-    console.log(msg)
-  }
+  // function handleOpen() {
+  //   console.log('WebSocket Open')
+  // }
+  // function handleClose() {
+  //   console.log('WebSocket Close')
+  // }
+  // function handleError() {
+  //   console.log('WebSocket Error')
+  // }
+  // function handleConnection(wss) {
+  //   wss.on('message', function(message) {
+  //     console.log("前端发过来的消息为"+ message)
+  //   })
+  // }
+  // function handleMessage() {
+  //   console.log(msg)
+  // }
   init()
-})(WebSocket)
+})()
