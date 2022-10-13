ee0d96bdef433e6b46aa20860bcf04dfbe52acc2
AaronCon <2905529185@qq.com>
2022-10-13 21:01:51 +0800

    完成最简易的聊天室

--- websocket/chat/js/index.js
+++ websocket/chat/js/index.js
@@ -1,24 +1,49 @@
-((doc, WebSocket) => {
+((doc, WebSocket, storage, location) => {
   const oList = doc.querySelector('#list')
   const oSend = doc.querySelector('#send')
   const ws = new WebSocket('ws://localhost:8000')
+  const name = storage.getItem('username')
 
   const init = () => {
+    // 判断是否登录，进行路由跳转
+    if(!name) {
+      return location.href = '/entry.html'
+    }
     bindEvent()
   }
   const bindEvent = () => {
+    // 添加点击发送命令
     oSend.addEventListener('click', handerEnterSend)
-    
-    function handerEnterSend() {
-      console.log("连接开始")
-      ws.onopen = function() {
-        let message = doc.querySelector('#message').value
-        console.log(message)
-        ws.send(message)
-      }()
+  }
+
+  const handerEnterSend = () => {
+    console.log("连接开始")
+    const data = {
+      name,
+      time: new Date().getTime(),
+      message: doc.querySelector('#message').value
+    }
+
+    ws.onopen = function() {
+      ws.send(JSON.stringify(data))
+    }()
+    ws.onmessage = function(msg) {
+      msg = JSON.parse(msg.data)
+      console.log(msg)
+      const { name, time, message } =  msg
+      console.log(name)
+      const oItem = doc.createElement('li')
+      oItem.innerHTML = `
+        <p>消息：${ message }</p>
+        <p>
+          <span>${ name }</span>
+          <i>${ new Date(time) }</i>
+        </p>
+      `
+      oList.appendChild(oItem)
     }
   }
 
   init()
-})(document, WebSocket)
+})(document, WebSocket, localStorage, location)
 
--- websocket/server/index.js
+++ websocket/server/index.js
@@ -7,31 +7,20 @@
   }
   function bindEvent() {
     ws.on('connection', function(wss) {
-      wss.on('message', function(message) {
-        console.log('客户端发送的消息为'+ message)
+      wss.on('message', (msg) => {
+        // console.log('' + msg)
+        // wss.send(''+ msg)
+        ws.clients.forEach((e) => {
+          e.send('' + msg)
+        })
       })
     })
-    // server.on('open', handleOpen)
-    // server.on('close', handleClose)
-    // server.on('error', handleError)
-    // ws.on('connection', handleConnection(wss))
   }
-  // function handleOpen() {
-  //   console.log('WebSocket Open')
+
+  // function handleMessage(msg) {
+  //   console.log(''+ msg)
+  //   // wss.send('fdsaf')
   // }
-  // function handleClose() {
-  //   console.log('WebSocket Close')
-  // }
-  // function handleError() {
-  //   console.log('WebSocket Error')
-  // }
-  // function handleConnection(wss) {
-  //   wss.on('message', function(message) {
-  //     console.log("前端发过来的消息为"+ message)
-  //   })
-  // }
-  // function handleMessage() {
-  //   console.log(msg)
-  // }
+
   init()
 })()
